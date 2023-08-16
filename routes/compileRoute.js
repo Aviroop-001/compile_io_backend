8d643ec718bf142c25e2c833120bbbbe5b5bf152
Aviroop-001 <banerjeeaviroop01@gmail.com>
2023-08-17 00:39:43 +0530

    Added run code with input

--- routes/compileRoute.js
+++ routes/compileRoute.js
@@ -4,10 +4,11 @@ const { exec } = require("child_process");
 const fs = require("fs");
 const path = require("path");
 
-router.post("/compile", (req, res) => {
-  const { language, code } = req.body;
 
-  const tempDir = path.join(__dirname, "temp"); // Construct the full path to the temp directory
+router.post("/compile", (req, res) => {
+  const {language, input, code} = req.body
+  
+  const tempDir = path.join(__dirname, "temp");
   if (!fs.existsSync(tempDir)) {
     fs.mkdirSync(tempDir);
   }
@@ -16,32 +17,40 @@ router.post("/compile", (req, res) => {
   const filePath = path.join(tempDir, fileName);
   fs.writeFileSync(filePath, code);
 
-  const compileCommand = `g++ ${filePath} -o ${path.join(__dirname, "temp","compiled_output")}`;
+  const compileCommand = `g++ ${filePath} -o ${path.join(tempDir,"compiled_output")}`;
 
-  // Execution
-  exec(compileCommand, (error, stdout, stderr) => {
-    if (error) {
-      console.error(`Compilation error: ${error}`);
-      res.status(500).json({ error: "Compilation failed", stderr });
+  // Compile
+  exec(compileCommand, (compileError, compileStdout, compileStderr) => {
+    if (compileError) {
+      console.error(`Compilation error: ${compileError}`);
+      res
+        .status(500)
+        .json({ error: "Compilation failed", stderr: compileStderr });
     } else {
       console.log("Compilation successful");
-      const runCommand = path.join(__dirname, "temp", "compiled_output");
-      // Execute the compiled program
-      exec(runCommand, (runError, runStdout, runStderr) => {
-        if (runError) {
-          console.error(`Execution error: ${runError}`);
-          res
-            .status(500)
-            .json({ error: "Execution failed", stderr: runStderr });
-        } else {
-          console.log("Execution successful");
-          res.json({ output: runStdout });
+
+      //Execute
+      const runCommand = path.join(tempDir, "compiled_output");
+      const childProcess = exec(
+        runCommand,
+        (runError, runStdout, runStderr) => {
+          if (runError) {
+            console.error(`Execution error: ${runError}`);
+            res
+              .status(500)
+              .json({ error: "Execution failed", stderr: runStderr });
+          } else {
+            console.log("Execution successful");
+            res.json({ output: runStdout });
+          }
         }
-      });
+      );
+
+      //user input
+      childProcess.stdin.write(input);
+      childProcess.stdin.end();
     }
   });
 });
-
-
 
 module.exports = router;
--- routes/temp/user_code.cpp
+++ routes/temp/user_code.cpp
@@ -1,7 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-cout<<"Hello world"<<endl;
-for(int i=0; i<5; i++) cout<<i<<"  "  ;
-return 0;
+    cout<<"Hello world"<<endl;
+    int n, e, k;
+    cin>>n;
+    vector<int> vec(n,0);
+    for(int i=0; i<n; i++){
+        cin>>e;
+        vec[i]=e;
+    }
+    for(auto i: vec)
+        cout<<i<<"\n";
+    cin>>k;
+    cout<<"This is k: "<<k<<endl;
+    return 0;
 }
